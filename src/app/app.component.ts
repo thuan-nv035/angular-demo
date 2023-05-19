8524ed331ddf19d4a690377c9c611c7c1f62b42e
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2023-05-19 16:57:20 +0700

    adD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  template: `
+  <main>
+  <a [routerLink]="['/']">
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+  </a>
+  <section class="content">
+    <router-outlet></router-outlet>
+  </section>
+</main>
+`,
+  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent, RouterModule],
 })
+
 export class AppComponent {
-  title = 'my-app';
+  title = 'homes';
 }
